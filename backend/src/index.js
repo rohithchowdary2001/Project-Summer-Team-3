4f556dc04eea69c4729f9bf58e625c16884ee27b
rohithchowdary2001 <rohithyadlapalli2001@gmail.com>
2025-06-17 14:19:07 -0500

    AWS

--- backend/src/index.js
+++ backend/src/index.js
@@ -86,6 +86,10 @@ app.use((err, req, res, next) => {
 // Get PORT from env or default 5000
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 async function startServer() {
   try {
     console.log('Connecting to database...');
